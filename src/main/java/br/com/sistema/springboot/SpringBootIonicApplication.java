a5a3d8a93f2ac562763bd4fa4f1aa494662eaaa7
huelton.santos <hueltondihon@gmail.com>
2017-11-09 23:31:46 -0200

    Criação do projeto

---
+++ src/main/java/br/com/sistema/springboot/SpringBootIonicApplication.java
@@ -0,0 +1,12 @@
+package br.com.sistema.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIonicApplication.class, args);
+	}
+}
