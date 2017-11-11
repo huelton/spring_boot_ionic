1556957dea5218390c40e86097126e5c7d9747fb
huelton.santos <hueltondihon@gmail.com>
2017-11-11 16:56:41 -0200

    Endpoint /pedidos/{id} disponibilizados

---
+++ src/main/java/br/com/sistema/springboot/services/PedidoService.java
@@ -0,0 +1,24 @@
+package br.com.sistema.springboot.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.sistema.springboot.domain.Pedido;
+import br.com.sistema.springboot.exceptions.ObjectNotFoundException;
+import br.com.sistema.springboot.respositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id){
+		Pedido obj = repo.findOne(id);
+		if(obj == null ){
+			throw new ObjectNotFoundException("Objeto nao encontrado! id: "+id
+					            +", tipo: "+Pedido.class.getName());
+		}
+		return obj;
+	}
+}
