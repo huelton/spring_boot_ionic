227c1b07fe98c16eed55fc9059329bc8231ee310
huelton.santos <hueltondihon@gmail.com>
2017-11-11 00:12:44 -0200

    Ajuste no endpoint /categorias/{id}

--- src/main/java/br/com/sistema/springboot/services/CategoriaService.java
+++ src/main/java/br/com/sistema/springboot/services/CategoriaService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.sistema.springboot.domain.Categoria;
+import br.com.sistema.springboot.exceptions.ObjectNotFoundException;
 import br.com.sistema.springboot.respositories.CategoriaRepository;
 
 @Service
@@ -14,7 +15,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id){
 		Categoria obj = repo.findOne(id);
-		
+		if(obj == null ){
+			throw new ObjectNotFoundException("Objeto nao encontrado! id: "+id
+					            +", tipo: "+Categoria.class.getName());
+		}
 		return obj;
 	}
 }
