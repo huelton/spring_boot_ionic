7d07ba991d29925fb98907b2d39aeb06f0653208
huelton.santos <hueltondihon@gmail.com>
2017-11-10 22:39:10 -0200

    Criando operacao de instanciação

--- src/main/java/br/com/sistema/springboot/SpringBootIonicApplication.java
+++ src/main/java/br/com/sistema/springboot/SpringBootIonicApplication.java
@@ -1,12 +1,31 @@
 package br.com.sistema.springboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootIonicApplication {
+import br.com.sistema.springboot.domain.Categoria;
+import br.com.sistema.springboot.respositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringBootIonicApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootIonicApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
+	}
+
 }
