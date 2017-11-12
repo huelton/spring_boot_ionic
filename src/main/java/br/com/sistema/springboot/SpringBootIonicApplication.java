176de4e18bd28e8995f45e4c521027236a33e398
huelton.santos <hueltondihon@gmail.com>
2017-11-12 12:53:26 -0200

    Paginação com parametros opcionais na Requisição

--- src/main/java/br/com/sistema/springboot/SpringBootIonicApplication.java
+++ src/main/java/br/com/sistema/springboot/SpringBootIonicApplication.java
@@ -67,9 +67,18 @@ public class SpringBootIonicApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... arg0) throws Exception {
+		
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Cama, Mesa e Banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
 		
+		categoriaRepository.save(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));	
+		
+		//
 		Produto p1 = new Produto(null,"Computador",2000.00);
 		Produto p2 = new Produto(null,"Impressora",800.00);
 		Produto p3 = new Produto(null,"Mouse",50.00);
@@ -80,8 +89,7 @@ public class SpringBootIonicApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
-		categoriaRepository.save(Arrays.asList(cat1,cat2));		
+					
 		produtoRepository.save(Arrays.asList(p1,p2,p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
