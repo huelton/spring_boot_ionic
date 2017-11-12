60598f564622810a0e04ace2f49870f440a4e1e7
huelton.santos <hueltondihon@gmail.com>
2017-11-12 15:00:26 -0200

    PUT,DELETE,GET para Cliente

--- src/main/java/br/com/sistema/springboot/resources/ClienteResource.java
+++ src/main/java/br/com/sistema/springboot/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package br.com.sistema.springboot.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.sistema.springboot.domain.Cliente;
+import br.com.sistema.springboot.dto.ClienteDTO;
 import br.com.sistema.springboot.services.ClienteService;
 
 @RestController
@@ -25,4 +34,57 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	
+	//INSERE UMA CATEGORIA
+/*		@RequestMapping(method=RequestMethod.POST)
+		public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto){
+			Cliente obj = ClienteService.fromDTO(objDto);
+			obj = ClienteService.insert(obj);
+			URI uri =ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+			
+			return ResponseEntity.created(uri).build();
+		}*/
+		
+		//ATUALIZA UMA CATEGORIA
+		@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+		public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
+			Cliente obj = ClienteService.fromDTO(objDto);
+			obj.setId(id);
+			obj = ClienteService.update(obj);
+			
+			return ResponseEntity.noContent().build();
+		}
+		
+		//DELETA UMA CATEGORIA
+		@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+		public ResponseEntity<Void> delete(@PathVariable Integer id){
+			ClienteService.delete(id);
+			
+			return ResponseEntity.noContent().build();		
+		}
+		
+		//RETORNA TODAS AS CATEGORIAS
+		@RequestMapping(method=RequestMethod.GET)
+		public ResponseEntity<List<ClienteDTO>> findAll(){
+			List <Cliente> list = ClienteService.findAll();
+			List <ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)
+					                                         ).collect(Collectors.toList()); //CONVERTE UMA LISTA PARA OUTRA LISTA
+			 return ResponseEntity.ok().body(listDto);
+			
+		}
+		
+		//PAGINAÇÃO DE CATEGORIAS
+		@RequestMapping(value="/page", method=RequestMethod.GET)
+		public ResponseEntity<Page<ClienteDTO>> findPage(
+				@RequestParam(value="page",defaultValue="0") Integer page, 
+				@RequestParam(value="linesPerPage",defaultValue="24") Integer linesPerPage, 
+				@RequestParam(value="orderBy",defaultValue="nome") String orderBy, 
+				@RequestParam(value="direction",defaultValue="ASC") String direction){
+			Page <Cliente> list = ClienteService.findPage(page, linesPerPage, orderBy, direction);
+			Page <ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj)); //CONVERTE UMA PAGE PARA OUTRA PAGE DTO
+			 return ResponseEntity.ok().body(listDto);
+			
+		}
+		
+	
 }
--- src/main/java/br/com/sistema/springboot/services/ClienteService.java
+++ src/main/java/br/com/sistema/springboot/services/ClienteService.java
@@ -1,10 +1,18 @@
 package br.com.sistema.springboot.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.sistema.springboot.domain.Cliente;
+import br.com.sistema.springboot.dto.ClienteDTO;
 import br.com.sistema.springboot.respositories.ClienteRepository;
+import br.com.sistema.springboot.services.exceptions.IntegrityViolationException;
 import br.com.sistema.springboot.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,4 +29,40 @@ public class ClienteService {
 		}
 		return obj;
 	}
+	
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());  //verifica se o id existe antes de atualizar
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+
+	public void delete(Integer id) {
+		find(id);  //verifica se o id existe antes de deletar
+		try{
+			repo.delete(id);
+		}catch(DataIntegrityViolationException e){
+			throw new IntegrityViolationException("Não é possivel excluir porque existem entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {		
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto){
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());		
+	}
+
 }
