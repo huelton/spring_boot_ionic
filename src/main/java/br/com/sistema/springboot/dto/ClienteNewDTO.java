6d32969db369c1a1014c56a19fa80fb70c40ae76
huelton.santos <hueltondihon@gmail.com>
2017-11-14 23:36:56 -0200

    Validação customizada de CPF ou CNPJ na inserção de Cliente

--- src/main/java/br/com/sistema/springboot/dto/ClienteNewDTO.java
+++ src/main/java/br/com/sistema/springboot/dto/ClienteNewDTO.java
@@ -2,22 +2,44 @@ package br.com.sistema.springboot.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import br.com.sistema.springboot.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-
-	private String nome;	
+	@NotEmpty(message="Preenchimento Obrigatório!")
+	@Length(min=5, max=120,message="O tamanho deve estar entre 5 e 120 caracteres!")	
+	private String nome;
+	
+	@NotEmpty(message="Preenchimento Obrigatório!")
+	@Email(message="Email Invalido!")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
